b2060850003fccefbefb78327d91274c6e0a24d9
HARI SHANKAR <charishankar30@gmail.com>
2025-02-22 23:05:16 +0530

    alignemt issue

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <footer className="bg-gray-900 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="flex flex-col md:flex-row justify-between items-center gap-10 md:gap-0">
-          <div className="text-center">
+          <div className="text-center w-screen md:px-0 px-5">
             <h3 className="text-2xl font-bold mb-4 text-yellow-400">SATTVA 2025</h3>
             <p className="text-gray-400 text-lg">
               Join us for the college arts and sports festival of the year.
